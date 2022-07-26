ad5060b43de835a50518c312356ab0205a77c770
zhangxiaojian <zhangxj@tuya.com>
2022-07-26 11:06:31 +0800

    feat: fixed st

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import { getAllDomNodes } from "./lib";
 class N2f {
   constructor(options) {
     this.options = options || {};
-    this.domArr = []
+    this.domArr = this.options.domArr || []
+    this.init();
   }
 
   init() {
-    getAllDomNodes(document.body, this.domArr, "INPUT")
+    getAllDomNodes(document.body, this.domArr, "INPUT");
+    console.log("this.domArr", this.domArr);
   }
 
   resetFocus(from = 0) {}
-
 }
 
 export default N2f;
--- src/lib.js
+++ src/lib.js
@@ -3,23 +3,37 @@
  * @param {*} root
  */
 
-export function getAllDomNodes(root = document.body, domArr = [], filterElementTagName) {
+export function getAllDomNodes(
+  root = document.body,
+  domArr = [],
+  filterElementTagName
+) {
   if (root) {
     // 元素节点
     if (root.nodeType === 1) {
       if (!filterElementTagName || root.tagName === filterElementTagName) {
         domArr.push(root);
+        root.style.border = '1px solid red'
       }
       if (root.hasChildNodes()) {
-        getAllDomNodes(root.childNodes[0]);
+        getAllDomNodes(root.childNodes[0], domArr, filterElementTagName);
       } else {
-        returnBySameRoute(root, getAllDomNodes, "nextElementSibling");
+        returnBySameRoute(
+          root,
+          { getAllDomNodes, domArr, filterElementTagName },
+          "nextElementSibling"
+        );
       }
     } else {
       if (root.nextElementSibling) {
-        getAllDomNodes(root.nextElementSibling);
+        getAllDomNodes(root.nextElementSibling, domArr, filterElementTagName);
       } else {
-        returnBySameRoute(root, getAllDomNodes, "nextElementSibling", "parentElement");
+        returnBySameRoute(
+          root,
+          { getAllDomNodes, domArr, filterElementTagName },
+          "nextElementSibling",
+          "parentElement"
+        );
       }
     }
   }
@@ -28,23 +42,24 @@ export function getAllDomNodes(root = document.body, domArr = [], filterElementT
 /**
  *
  * @param {*} root
- * @param {*} searchApi
+ * @param {*} param1
  * @param {*} $1
  * @param {*} $2
  */
 export function returnBySameRoute(
   root,
-  searchApi,
+  { getAllDomNodes, domArr, filterElementTagName },
   $1 = "nextElementSibling",
-  $2 = "parentElement"
+  $2
 ) {
-  let _root = root;
+  let _root = root,
+    $3 = null;
   // eslint-disable-next-line no-constant-condition
   while (true) {
     // 返回上级节点，直至HTML
     if (_root.tagName === "HTML") break;
     if (($3 = $2 ? _root[$2][$1] : _root[$1])) {
-      searchApi($3);
+      getAllDomNodes($3, domArr, filterElementTagName);
       break;
     } else {
       _root = _root.parentElement;
